8addb568e272ec93f6639ed885aa670f7dfdcede
JohnyKapo <janikkapral1234@gmail.com>
2024-03-07 13:24:05 +0100

    added filter options for brand and model connected to backend endpoint

--- car-hut-web/src/components/mainpage/ClickAndPick.js
+++ car-hut-web/src/components/mainpage/ClickAndPick.js
@@ -5,35 +5,35 @@ function ClickAndPick() {
         <div className="section-body-click-and-pick">
             <div className='section-header-text-click-and-pick'>Click and pick!</div>
             <div className='content-click-and-pick'>
-                <div class="row-click-and-pick">
-                    <div class="cell-click-and-pick">
+                <div className="row-click-and-pick">
+                    <div className="cell-click-and-pick">
                         <img className='img-click-and-pick' src={require("../../images/carbrands/audi.png")} alt="audi"/>
                     </div>
-                    <div class="cell-click-and-pick">
+                    <div className="cell-click-and-pick">
                         <img className='img-click-and-pick' src={require("../../images/carbrands/bmw.png")} alt="bmw"/>
                     </div>
-                    <div class="cell-click-and-pick">
+                    <div className="cell-click-and-pick">
                         <img className='img-click-and-pick' src={require("../../images/carbrands/honda.png")} alt="honda"/>
                     </div>
-                    <div class="cell-click-and-pick">
+                    <div className="cell-click-and-pick">
                         <img className='img-click-and-pick' src={require("../../images/carbrands/kia.png")} alt="kia"/>
                     </div>
-                    <div class="cell-click-and-pick">
+                    <div className="cell-click-and-pick">
                         <img className='img-click-and-pick' src={require("../../images/carbrands/mercedes.png")} alt="mercedes"/>
                     </div>
-                    <div class="cell-click-and-pick">
+                    <div className="cell-click-and-pick">
                         <img className='img-click-and-pick' src={require("../../images/carbrands/skoda.png")} alt="skoda"/>
                     </div>
-                    <div class="cell-click-and-pick">
+                    <div className="cell-click-and-pick">
                         <img className='img-click-and-pick' src={require("../../images/carbrands/tesla.png")} alt="tesla"/>
                     </div>
-                    <div class="cell-click-and-pick">
+                    <div className="cell-click-and-pick">
                         <img className='img-click-and-pick' src={require("../../images/carbrands/toyota.png")} alt="toyota"/>
                     </div>
-                    <div class="cell-click-and-pick">
+                    <div className="cell-click-and-pick">
                         <img className='img-click-and-pick' src={require("../../images/carbrands/volvo.png")} alt="volvo"/>
                     </div>
-                    <div class="cell-click-and-pick">
+                    <div className="cell-click-and-pick">
                         <img className='img-click-and-pick' src={require("../../images/carbrands/vw.png")} alt="vw"/>
                     </div>
                 </div>
--- car-hut-web/src/components/mainpage/FilterSearch.js
+++ car-hut-web/src/components/mainpage/FilterSearch.js
@@ -1,6 +1,33 @@
 import '../../css/FilterSearch.css'
+import React, { useState, useEffect } from 'react';
 
 function FilterSearch() {
+    const [brands, setBrands] = useState([]);
+    const [models, setModels] = useState([]);
+    const [selectedBrand, setSelectedBrand] = useState('');
+    const [selectedModel, setSelectedModel] = useState('');
+
+    useEffect(() => {
+        // Fetch data from the API when the component mounts
+        fetch('http://localhost:8080/api/getAllBrands')
+            .then(response => response.json())
+            .then(data => setBrands(data))
+            .catch(error => console.error('Error fetching brands:', error));
+    }, []);
+
+    useEffect(() => {
+        // Fetch models when a brand is selected
+        if (selectedBrand) {
+            fetch(`http://localhost:8080/api/getModelsByBrand/${selectedBrand}`)
+                .then(response => response.json())
+                .then(data => setModels(data))
+                .catch(error => console.error('Error fetching models:', error));
+        } else {
+            // Clear models when no brand is selected
+            setModels([]);
+        }
+    }, [selectedBrand]);
+
     return (
         <div className='section-body'>
             <img src={require('../../images/mainpage/find_car.png')}/>
@@ -8,28 +35,30 @@ function FilterSearch() {
                 <div className='comboboxes'>
                     <div className='combobox-entity'>
                         <div className='label'>Brand</div>
-                        <div class="custom-combobox">
-                            <select id="myComboBox">
-                                <option value="option1">Option 1</option>
-                                <option value="option2">Option 2</option>
-                                <option value="option3">Option 3</option>
+                        <div className="custom-combobox">
+                            <select id="brandComboBox" className='myComboBox' value={selectedBrand} onChange={(e) => setSelectedBrand(e.target.value)}>
+                                <option value="" disabled>Select Brand</option>
+                                {brands.map(brand => (
+                                    <option key={brand.id} value={brand.brand}>{brand.brand}</option>
+                                ))}
                             </select>
                         </div>
                     </div>
                     <div className='combobox-entity'>
                         <div className='label'>Model</div>
-                        <div class="custom-combobox">
-                            <select id="myComboBox">
-                                <option value="option1">Option 1</option>
-                                <option value="option2">Option 2</option>
-                                <option value="option3">Option 3</option>
+                        <div className="custom-combobox">
+                            <select id="modelComboBox" className={`myComboBox ${!selectedBrand ? 'disabled' : ''}`} value={selectedModel} onChange={(e) => setSelectedModel(e.target.value)} disabled={!selectedBrand}>
+                                <option value="" disabled={!selectedBrand}>Select Model</option>
+                                {models.map(model => (
+                                    <option key={model.id} value={model.model}>{model.model}</option>
+                                ))}
                             </select>
                         </div>
                     </div>
                     <div className='combobox-entity'>
                         <div className='label'>Price</div>
-                        <div class="custom-combobox">
-                            <select id="myComboBox">
+                        <div className="custom-combobox">
+                            <select id="priceComboBox" className='myComboBox'>
                                 <option value="option1">Option 1</option>
                                 <option value="option2">Option 2</option>
                                 <option value="option3">Option 3</option>
@@ -38,8 +67,8 @@ function FilterSearch() {
                     </div>
                     <div className='combobox-entity'>
                         <div className='label'>Mileage</div>
-                        <div class="custom-combobox">
-                            <select id="myComboBox">
+                        <div className="custom-combobox">
+                            <select id="mileageComboBox" className='myComboBox'>
                                 <option value="option1">Option 1</option>
                                 <option value="option2">Option 2</option>
                                 <option value="option3">Option 3</option>
